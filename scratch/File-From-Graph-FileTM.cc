2144de8886bab3b3097b604de884d2920d0b9117
Vipul Harsh <vipulharsh.iit@gmail.com>
2018-12-09 17:50:37 -0600

    cleaning up File-From-Graph-FileTM.cc

--- scratch/File-From-Graph-FileTM.cc
+++ scratch/File-From-Graph-FileTM.cc
@@ -19,6 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * Authors: Linh Vu <linhvnl89@gmail.com>, Daji Wong <wong0204@e.ntu.edu.sg>
+ * Modifiedy by: Vipul Harsh <vipulharsh93@gmail.com>
  */
 
 #include <iostream>
@@ -46,27 +47,17 @@
 #include "ns3/ipv4-routing-table-entry.h"
 
 /*
-	- This work goes along with the paper "Towards Reproducible Performance Studies of Datacenter Network Architectures Using An Open-Source Simulation Approach"
+	- Adapted from the paper "Towards Reproducible Performance Studies of Datacenter Network Architectures Using An Open-Source Simulation Approach"
 
 	- The code is constructed in the following order:
-		1. Creation of Node Containers 
-		2. Initialize settings for On/Off Application
-		3. Connect hosts to edge switches
-		4. Connect edge switches to aggregate switches
-		5. Connect aggregate switches to core switches
+		1. Creation of Node Containers from a topology file
+		2. Initialize settings for On/Off Application/bulk.send application, traffic matrix read from a file
+		3. Connect hosts to top of the rack switches
+		4. Connect network switch using edges read from the topology file
 		6. Start Simulation
 
-	- Addressing scheme:
-		1. Address of host: 10.pod.switch.0 /24
-		2. Address of edge and aggregation switch: 10.pod.switch.0 /16
-		3. Address of core switch: 10.(group number + k).switch.0 /8
-		   (Note: there are k/2 group of core switch)
-
-	- On/Off Traffic of the simulation: addresses of client and server are randomly selected everytime
 	
-	- Simulation Settings:
-                - Number of pods (k): 4-24 (run the simulation with varying values of k)
-                - Number of nodes: 16-3456
+	- Simulation Settings, along with examples:
 		- Simulation running time: 100 seconds
 		- Packet size: 1024 bytes
 		- Data rate for packet sending: 1 Mbps
@@ -74,7 +65,6 @@
 		- Delay time for device: 0.001 ms
 		- Communication pairs selection: Random Selection with uniform probability
 		- Traffic flow pattern: Exponential random traffic
-		- Routing protocol: Nix-Vector
 
         - Statistics Output:
                 - Flowmonitor XML output file: Fat-tree.xml is located in the /statistics folder
@@ -88,7 +78,7 @@ NS_LOG_COMPONENT_DEFINE ("Fat-Tree-Architecture");
 
 // Function to create address string from numbers
 //
-char * toString(int a,int b, int c, int d){
+char * ipOctectsToString(int a,int b, int c, int d){
 
 	int first = a;
 	int second = b;
@@ -130,14 +120,14 @@ int
    int nreqarg = 5;
    if(argc <= nreqarg){
       cout<<nreqarg-1<<" arguments required, "<<argc-1<<" given."<<endl;
-      cout<<"Usage> <exec> <topology_file> <tm_file> <result_file> <drop queue limit(e.g. 100 for a limit of 100 packets)> <background data rate (e.g. 10 for 10 Mbps) >"<<endl;
+      cout<<"Usage> <exec> <topology_file> <tm_file> <result_file> <drop queue limit(e.g. 100 for a limit of 100 packets)> <data rate for on/off (e.g. 10 for 10 Mbps) >"<<endl;
       exit(0);
    }
    string topology_filename = argv[1];
    string tm_filename = argv[2];
    string result_filename = argv[3];
    int drop_queue_limit = atoi(argv[4]);
-   string background_datarate = argv[5];
+   string on_off_datarate = argv[5];
 
 
    cout<<"Running topology: "<<topology_filename<<", output result to "<<result_filename<<endl;
@@ -160,10 +150,6 @@ int
    static map<int, int> hostToTor;
 	//char filename [] = "statistics/File-From-Graph.xml";// filename for Flow Monitor xml output file
    //string topology_filename = "topology/ns3_deg4_sw8_svr8_os1_i1.edgelist";
-   //string topology_filename = "topology/ns3fat_deg6_sw45_svr54_os4_i1.edgelist";
-   //string topology_filename = "topology/ns3_deg10_sw125_svr250_os4_i1.edgelist";
-   //string topology_filename = "topology/ns3fat_deg10_sw125_svr250_os4_i1.edgelist";
-   //string topology_filename = "topology/line.topo";
    
    class topologyUtility{
       public:
@@ -247,35 +233,19 @@ int
                }
             }
          }
-
-/*
-         static char* getHostIpAddress(int host){
-            int rack = getHostRack(host);
-            int ind = getHostIndexInRack(host);
-            return toString(10, getFirstOctetOfTor(rack), getSecondOctetOfTor(rack), ind + 2);
-         }
-         static char* getRackBaseIpAddress(int rack){
-            return toString(10, getFirstOctetOfTor(rack), getSecondOctetOfTor(rack), 0);
-         }
          static pair<char*, char*> getLinkBaseIpAddress(int sw, int h){
-            char* subnet = toString(10, getFirstOctetOfTor(sw), getSecondOctetOfTor(sw), 0);
-            char* base = toString(0, 0, 0, 1 + getNumHostsInRack(sw)*2 + (h*2)); 
-            return pair<char*, char*>(subnet, base);
-         }
-*/
-         static pair<char*, char*> getLinkBaseIpAddress(int sw, int h){
-            char* subnet = toString(10, (getFirstOctetOfTor(sw) | 128), getSecondOctetOfTor(sw), 0);
-            char* base = toString(0, 0, 0, 2 + getNumHostsInRack(sw) + 2*h); 
+            char* subnet = ipOctectsToString(10, (getFirstOctetOfTor(sw) | 128), getSecondOctetOfTor(sw), 0);
+            char* base = ipOctectsToString(0, 0, 0, 2 + getNumHostsInRack(sw) + 2*h); 
             return pair<char*, char*>(subnet, base);
          }
          static char* getHostIpAddress(int host){
             int rack = getHostRack(host);
             int ind = getHostIndexInRack(host);
-            return toString(10, getFirstOctetOfTor(rack), getSecondOctetOfTor(rack), ind*2 + 2);
+            return ipOctectsToString(10, getFirstOctetOfTor(rack), getSecondOctetOfTor(rack), ind*2 + 2);
          }
          static pair<char*, char*> getHostBaseIpAddress(int sw, int h){
-            char* subnet = toString(10, getFirstOctetOfTor(sw), getSecondOctetOfTor(sw), 0);
-            char* base = toString(0, 0, 0, (h*2) + 1); 
+            char* subnet = ipOctectsToString(10, getFirstOctetOfTor(sw), getSecondOctetOfTor(sw), 0);
+            char* base = ipOctectsToString(0, 0, 0, (h*2) + 1); 
             return pair<char*, char*>(subnet, base);
          }
          static pair<int, int> getRackHostsLimit(int rack){ //rack contains hosts from [l,r) ===> return (l, r)
@@ -306,24 +276,14 @@ int
    topologyUtility::readTopologyFromFile(topology_filename);
    topologyUtility::readServerTmFromFile(tm_filename);
 
-// Define variables for On/Off Application
-// These values will be used to serve the purpose that addresses of server and client are selected randomly
-// Note: the format of host's address is 10.pod.switch.(host+2)
-//
-
-
-// Initialize other variables
-//
-
-// Initialize parameters for On/Off background_application
+// Initialize parameters for On/Off application
 //
 	int port = 9; //port for background app
-   int fg_port = 10; //port for foreground app
 	int packetSize = 1024;		// 1024 bytes
-	string dataRate_OnOff = background_datarate + "Mbps"; //"1Mbps";
+	string dataRate_OnOff = on_off_datarate + "Mbps"; //"1Mbps";
 	char maxBytes [] = "0" ; //"50000"; // "0";		// unlimited
 
-// Initialize parameters for Csma and PointToPoint protocol
+// Initialize parameters for PointToPoint protocol
 //
 	char dataRate [] = "1000Mbps";	// 1Gbps
 	float delay = 0.001;		// 0.001 ms
@@ -333,7 +293,7 @@ int
 // Output some useful information
 //	
 	std::cout << "Total number of hosts =  "<< total_host<<"\n";
-   std::cout << "Background flow data rate = "<< dataRate_OnOff<<"."<<endl;
+   std::cout << "On/Off flow data rate = "<< dataRate_OnOff<<"."<<endl;
 	std::cout << "------------- "<<"\n";
 
 // Initialize Internet Stack and Routing Protocols
@@ -354,18 +314,11 @@ int
    tors.Create(num_tor);
    internet.Install(tors);
 
-/*
-	NodeContainer bridges;				// NodeContainer for all ToR bridges
-   bridges.Create(num_tor);
-   internet.Install(bridges);
-*/
-
    NodeContainer rackhosts[num_tor];
 	for (int i=0; i<num_tor;i++){  	
 		rackhosts[i].Create (hostsInTor[i].size());
 		internet.Install (rackhosts[i]);		
 	}
-
 
 //=========== Initialize settings for On/Off Application ===========//
 //
@@ -373,20 +326,19 @@ int
 // Generate traffics for the simulation
 //
 
-   cout<<"Creating background application .... "<<endl;
+   cout<<"Creating application flows .... "<<endl;
 
    double max_traffic = 0.0;
    for(int i=0; i<total_host; i++){
       //cout<<"row.size(): "<<serverTM[i].size()<<endl;
       max_traffic = max(max_traffic, *std::max_element(serverTM[i].begin(),serverTM[i].end()));
    }
-   double traffic_wt = 0.0004;
+   double traffic_wt = 0.1;
    cout<<"Max Traffic: "<<max_traffic<<", Weighted: "<<max_traffic * traffic_wt<<endl;
 
-   cout<<"size of application container "<<sizeof(ApplicationContainer)<<" bytes"<<endl;
-	ApplicationContainer** background_app = new ApplicationContainer*[total_host];
+	ApplicationContainer** flow_app = new ApplicationContainer*[total_host];
    for(int i=0; i<total_host; i++)
-      background_app[i] = new ApplicationContainer[total_host];
+      flow_app[i] = new ApplicationContainer[total_host];
    int nflows = 0, total_bytes=0;
    for(int i=0; i<total_host; i++){
       for(int j=0; j<total_host; j++){
@@ -398,16 +350,13 @@ int
          char* bytes_c = new char[50];
          sprintf(bytes_c,"%d",bytes);
          string bytes_s = bytes_c;
-         if(i%100 == 0 && j%100 == 0)
-            cout<<"Bytes = "<<bytes_c<<endl;
          char *add = topologyUtility::getHostIpAddress(i);
 
          OnOffHelper oo = OnOffHelper("ns3::TcpSocketFactory",Address(InetSocketAddress(Ipv4Address(add), port))); // ip address of server
          BulkSendHelper source ("ns3::TcpSocketFactory", Address(InetSocketAddress(Ipv4Address(add), port)));
          // Initialize On/Off Application with addresss of server
          if(onOffApplication){
-            cout<<"On off application: shouldn't come here"<<endl;
-            exit(0);
+            cout<<"Initializing On Off flow"<<endl;
             double onTime = 0.010, offTime = 0.010;
             oo.SetAttribute("OnTime",RandomVariableValue(ExponentialVariable(onTime)));  
             oo.SetAttribute("OffTime",RandomVariableValue(ExponentialVariable(offTime))); 
@@ -415,7 +364,7 @@ int
             oo.SetAttribute("DataRate",StringValue (dataRate_OnOff));      
             oo.SetAttribute("MaxBytes",StringValue (bytes_c));
          }
-         //Initialize BulkSend background_application
+         //Initialize BulkSend application
          else{
             // Set the amount of data to send in bytes.  Zero is unlimited.
             source.SetAttribute("MaxBytes",StringValue (bytes_c));
@@ -426,16 +375,16 @@ int
          if(onOffApplication){
             NodeContainer onoff;
             onoff.Add(rackhosts[jrack].Get(topologyUtility::getHostIndexInRack(j)));
-            background_app[i][j] = oo.Install (onoff);
+            flow_app[i][j] = oo.Install (onoff);
             //std::cout << "Finished creating On/Off traffic"<<"\n";
          }
          //For bulk send
          else{
             NodeContainer bulksend;
             bulksend.Add(rackhosts[jrack].Get(topologyUtility::getHostIndexInRack(j)));
-            background_app[i][j] = source.Install (bulksend);
-            //background_app[i][j].Start (Seconds (1.0));
-            //background_app[i][j].Stop (Seconds (simTimeInSec));
+            flow_app[i][j] = source.Install (bulksend);
+            //flow_app[i][j].Start (Seconds (1.0));
+            //flow_app[i][j].Stop (Seconds (simTimeInSec));
             //std::cout << "Finished creating Bulk send and Packet Sink Applications"<<"\n";
          }
       }
@@ -443,21 +392,15 @@ int
    cout<<"Total Flows in the system: "<<nflows<<" carrying "<<total_bytes<<" bytes"<<endl;
 
    for(int i=0;i<total_host; i++){
-      //Create packet sink background_application on every server
+      //Create packet sink application on every server
       PacketSinkHelper sink ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), port));
       int irack = topologyUtility::getHostRack(i);
       ApplicationContainer sinkApps = sink.Install (rackhosts[irack].Get(topologyUtility::getHostIndexInRack(i)));
       sinkApps.Start (Seconds (0.0));
       sinkApps.Stop (Seconds (simTimeInSec));
-
-      //Create one packet sink for foreground_application 
-      PacketSinkHelper fg_sink ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), fg_port));
-      sinkApps = fg_sink.Install (rackhosts[irack].Get(topologyUtility::getHostIndexInRack(i)));
-      sinkApps.Start (Seconds (0.0));
-      sinkApps.Stop (Seconds (simTimeInSec));
    }
 
-   cout<<"Finished creating background application"<<endl;
+   cout<<"Finished creating applications"<<endl;
 
    
 // Inintialize Address Helper
@@ -470,46 +413,8 @@ int
   	p2p.SetDeviceAttribute ("DataRate", StringValue (dataRate));
   	p2p.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (delay)));
 
-
-// Initialize Csma helper
-//
-  	CsmaHelper csma;
-  	csma.SetChannelAttribute ("DataRate", StringValue (dataRate));
-  	csma.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (delay)));
 //=========== Connect switches to hosts ===========//
 //
-
-/*
-	NetDeviceContainer hostSw[num_tor];		
-	NetDeviceContainer bridgeDevices[num_tor];	
-	Ipv4InterfaceContainer ipContainer[num_tor];
-   for (int i=0;i<num_tor;i++){
-			NetDeviceContainer link1 = csma.Install(NodeContainer (tors.Get(i), bridges.Get(i)));
-			hostSw[i].Add(link1.Get(0));				
-			bridgeDevices[i].Add(link1.Get(1));			
-         for (int h=0; h<topologyUtility::getNumHostsInRack(i); h++){			
-            //cout<<"Creating net device for host: "<<h<<" --> "<<i<<endl;
-				NetDeviceContainer link2 = csma.Install(NodeContainer (rackhosts[i].Get(h), bridges.Get(i)));
-				hostSw[i].Add(link2.Get(0));			
-				bridgeDevices[i].Add(link2.Get(1));						
-         }
-			BridgeHelper bHelper;
-			bHelper.Install (bridges.Get(i), bridgeDevices[i]);
-			//Assign address
-			char *subnet = topologyUtility::getRackBaseIpAddress(i);
-         //cout<<"Subnet: "<<subnet<<" for "<< hostSw[i].GetN()<<endl;
-			address.SetBase (subnet, "255.255.255.0");
-			ipContainer[i] = address.Assign(hostSw[i]);			
-         //
-         //for (it = ipContainer[i].Begin (); it != ipContainer[i].End (); ++it)
-         //{
-         //   std::pair<Ptr<Ipv4>, uint32_t> ret = *it;
-         //   ret.first->GetAddress(0,0).GetBroadcast().Print(cout);
-         //}
-         //
-   }
-*/
-
 
 	vector<NetDeviceContainer> sh[num_tor]; 	
 	vector<Ipv4InterfaceContainer> ipShContainer[num_tor];
@@ -574,9 +479,9 @@ int
       for (int j=0;j<total_host;j++){
          int bytes = truncateBytes((int)(serverTM[i][j] * traffic_wt));
          if(bytes < 1) continue;
-		   //background_app[i][j].Start (Seconds (1.0));
-		   background_app[i][j].Start (Seconds (rand() * 100.0/RAND_MAX));
-  		   background_app[i][j].Stop (Seconds (simTimeInSec));
+		   //flow_app[i][j].Start (Seconds (1.0));
+		   flow_app[i][j].Start (Seconds (rand() * 100.0/RAND_MAX));
+  		   flow_app[i][j].Stop (Seconds (simTimeInSec));
       }
 	}
 
--- statistics/analyse.py
+++ statistics/analyse.py
@@ -2,10 +2,18 @@ import xml.etree.ElementTree as ET
 import sys
 
 if(len(sys.argv) < 2):
-   print "Usage: python analyse.py <xml file>"
+   print "Usage: python analyse.py <xml file> <output cdf (optional)> <'bg_flows', optional, default='fg_flows')"
    sys.exit()
 
 xmlfile = sys.argv[1]
+
+outputcdf = False
+if(len(sys.argv) > 2 and sys.argv[2] == "cdf"):
+   outputcdf = True
+
+consider_bg_flows = False
+if(len(sys.argv) > 3 and sys.argv[3] == "bg_flows"):
+   consider_bg_flows = True
 
 tree = ET.parse(xmlfile)
 root = tree.getroot()
@@ -18,7 +26,6 @@ bg_port = '9'
 fg_port = '10'
 
 for child in root:
-   #print child.tag, child.attrib
    if(child.tag == 'Ipv4FlowClassifier'):
       for flow in child:
          #print flow.tag, flow.attrib
@@ -27,8 +34,8 @@ for child in root:
          elif(flow.attrib['sourcePort']==bg_port):
             bg_flowids.append(flow.attrib['flowId'])
 
-#print "|bg_flows|: ", len(bg_flowids)
-#print "|fg_flows|: ", len(fg_flowids)
+print "|bg_flows|: ", len(bg_flowids)
+print "|fg_flows|: ", len(fg_flowids)
 
 #print "done 1"
 
@@ -38,12 +45,15 @@ def getTimeFromLiteralInNS(lit):
    return float(lit)
 
 #print "done 2"
+flow_ids = fg_flowids
+if(consider_bg_flows == True):
+   flow_ids = bg_flowids
 
 flow_times = []
 for child in root:
    if(child.tag == 'FlowStats'):
       for flow in child:
-         if(flow.attrib['flowId'] in fg_flowids):
+         if(flow.attrib['flowId'] in flow_ids):
            first_tx_time = flow.attrib['timeFirstTxPacket'] 
            last_rx_time = flow.attrib['timeLastRxPacket']
            fctInMicrosec = getTimeFromLiteralInNS(last_rx_time) / 1000.0
@@ -54,10 +64,23 @@ for child in root:
            #    app_completion_time = fctInMicrosec
 
 flow_times.sort()
-app_completion_time = flow_times[-1]
-ile_99 = flow_times[(99 * len(flow_times) + 99)/100 - 1]
-avg_fct = sum(flow_times)/len(flow_times)
-print len(flow_times), avg_fct, ile_99, app_completion_time
+if(outputcdf):
+   flow_times = [x/1000.0 - 1000.0 for x in flow_times]
+   for x in flow_times:
+      print x
+else:
+   app_completion_time = flow_times[-1]
+   ile_50 = (flow_times[(50 * len(flow_times) + 50)/100 - 1] - 1000000.0)/1000.0
+   ile_90 = (flow_times[(90 * len(flow_times) + 90)/100 - 1] - 1000000.0)/1000.0
+   ile_99 = (flow_times[(99 * len(flow_times) + 99)/100 - 1] - 1000000.0)/1000.0
+   avg_fct = (sum(flow_times)/len(flow_times) - 1000000.0)/1000.0
+   #print len(flow_times), avg_fct, ile_50, ile_99, app_completion_time
+   print "num flows: ", len(flow_times)
+   print "50%ile fct (us): ", ile_50
+   print "90%ile fct (us): ", ile_90
+   print "99%ile fct (us): ", ile_99
+   print "Avg fct (us): ", avg_fct
+
 #print "Numflows: ", len(flow_times)
 #print "100 %ile time: ", app_completion_time
 #print "99 %ile time: ", ile_99
